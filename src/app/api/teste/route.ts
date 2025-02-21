543eef072efdf2837bf273336faf2004ed745f6e
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2025-02-21 19:04:34 +0000

    Test API

--- src/app/api/teste/route.ts
+++ src/app/api/teste/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from "next/server";
+import dados from "@/data.json"
 
 export async function GET() {
-    return NextResponse.json({"mensagem" : "Olá REST!!!"});
+    return NextResponse.json(dados);
+}
+
+export async function POST(request: Request) {
+    const data = await request.json()
+    dados.push(data)
+    return NextResponse.json(dados);
 }
